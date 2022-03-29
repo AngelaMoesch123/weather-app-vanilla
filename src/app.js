7ac49bbefe92edb34beab2c5c61ae247bfab9e2f
AngelaMoesch123 <angelarosesnyder08@gmail.com>
2022-03-28 20:44:20 -0700

    update JS

--- src/app.js
+++ src/app.js
@@ -67,6 +67,13 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "9fcea006200cbfdcbad380c282e76781";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
